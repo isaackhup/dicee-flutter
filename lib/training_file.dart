a3d480ec2f448805aa931ed1bfd32fa24a41d4b4
isaackhup <isaacmunghaukhup@gmail.com>
2025-05-06 13:04:11 -0500

    new file added: main_code_6.dart, it is making both of the two dicee to modify numbes when it is on pressed

--- lib/training_file.dart
+++ lib/training_file.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,15 +7,15 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         appBar: AppBar(
-          title: Text('Dicee',
-          ),
+          title: Text('Dicee'),
           backgroundColor: Colors.purple,
         ),
         body: DicePage(),
       ),
-    )
+    ),
   );
 }
+
 class DicePage extends StatefulWidget {
   @override
   State<DicePage> createState() => _DicePageState();
@@ -22,30 +23,33 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
-  
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
-        children: <Widget>[
+        children: [
           Expanded(
-            //FlatButton() is deprecated. It is replaced by TextButton. Date: 5/3/2025
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = 6;
-                  print('diceNumber = $leftDiceNumber');
-                });
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-            ),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    print('diceNumber = $leftDiceNumber');
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
           ),
           Expanded(
             child: TextButton(
               onPressed: () {
-                print('Right button got pressed.');
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                  print('diceNumber = $rightDiceNumber');
+                });
               },
-              child: Image.asset('images/dice1.png'),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
@@ -53,3 +57,5 @@ class _DicePageState extends State<DicePage> {
     );
   }
 }
+
+
